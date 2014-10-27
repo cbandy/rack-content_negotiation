6bf923d7424317dfbeb3f8356b950e9262716720
Chris Bandy <bandy.chris@gmail.com>
2014-10-27 03:36:05 +0000

    Use one instance to handle one header

--- lib/rack/content_negotiation.rb
+++ lib/rack/content_negotiation.rb
@@ -1,46 +1,63 @@
 require 'rack/accept_headers'
 
-module Rack
-  class ContentNegotiation
-    attr_accessor :header_name, :match_unacceptable
-    attr_reader :application, :available, :order
+##
+#
+# use Rack::ContentNegotiation, :media_type do |negotiate|
+#   negotiate.on('application/hal+json', 'application/json') do |env|
+#   end
+#
+#   negotiate.on('text/html', &html_application)
+# end
+#
+module Rack::ContentNegotiation
 
-    def initialize(application)
-      @application = application
-      @available = Hash.new
-      @header_name = :media_type
-      @order = Array.new
-      yield self if block_given?
+  def self.new(application, header_name = :media_type, match_unacceptable = false)
+    Middleware.new(application, header_name, match_unacceptable).tap { |m| yield m if block_given? }
+  end
+
+  class Matcher
+    attr_reader :available, :header_name, :match_unacceptable
+
+    def initialize(header_name, match_unacceptable)
+      @available = Array.new
+      @header_name = header_name
+      @match_unacceptable = match_unacceptable
     end
 
-    [ :charset, :encoding, :language, :media_type ].each do |header_name|
-      define_method(header_name) do |match_unacceptable = false|
-        @header_name = header_name
-        @match_unacceptable = match_unacceptable
-      end
+    def call(accept_headers)
+      accept_headers.__send__(@header_name).best_of(@available, @match_unacceptable)
+    end
+  end
+
+  class Middleware
+    attr_reader :application, :available
+
+    def initialize(application, header_name, match_unacceptable)
+      @application = application
+      @available = Hash.new(application)
+      @matcher = Matcher.new(header_name, match_unacceptable)
     end
 
     def call(env)
       headers = env['rack-accept_headers.request']
 
       if headers
-        header = headers.__send__(@header_name)
-        order = @order.empty? ? @available.keys : @order
-
-        @available.fetch(
-          env["rack-content_negotiation.#{@header_name}"] =
-          header.best_of(order, @match_unacceptable),
-          @application
-        ).call(env)
+        match = env["rack-content_negotiation.#{@matcher.header_name}"] = @matcher.call(headers)
+        @available[match].call(env)
       else
         Rack::AcceptHeaders.new(self).call(env)
       end
     end
 
+    def for(*values)
+      @matcher.available.concat(values)
+    end
+
     def on(*values, &block)
       application = block_given? ? block : values.pop
-      values.each { |value| @available[value] ||= application }
-      @order.concat(values)
+      values.each { |value| @available[value] = application }
+      @matcher.available.concat(values)
     end
   end
+
 end
--- spec/rack/content_negotiation_spec.rb
+++ spec/rack/content_negotiation_spec.rb
@@ -1,38 +1,25 @@
 require 'rack/content_negotiation'
 
 RSpec.describe Rack::ContentNegotiation do
+  let(:default) { double(:default) }
   let(:env) { Hash.new }
-  let(:fallback) { double(:fallback) }
 
-  subject(:middleware) { described_class.new(fallback) }
+  subject(:middleware) { described_class.new(default) }
 
   context 'when not configured' do
     let(:result) { double }
 
-    it 'calls the fallback application' do
-      expect(fallback).to receive(:call).with(env).and_return(result)
+    it 'calls the default application' do
+      expect(default).to receive(:call).with(env).and_return(result)
       expect(middleware.call(env)).to be(result)
     end
   end
 
   describe 'configuration' do
-    it 'has a method to set the header name/type' do
-      middleware.charset
-      expect(middleware.header_name).to eq(:charset)
-
-      middleware.encoding
-      expect(middleware.header_name).to eq(:encoding)
-
-      middleware.language
-      expect(middleware.header_name).to eq(:language)
-
-      middleware.media_type
-      expect(middleware.header_name).to eq(:media_type)
-    end
+    let(:result) { double }
 
     it 'has a method to add an available application as the last argument' do
       called = false
-      result = double
       application = lambda do |arg|
         called = true
         expect(arg).to be env
@@ -47,7 +34,6 @@ RSpec.describe Rack::ContentNegotiation do
 
     it 'has a method to add an available application as a block' do
       called = false
-      result = double
 
       middleware.on('text/html') do |arg|
         called = true
@@ -58,21 +44,49 @@ RSpec.describe Rack::ContentNegotiation do
       expect(middleware.call(env)).to be result
       expect(called).to be true
     end
+  end
 
-    it 'can be configured without application preference' do
-      result = double
-      middleware.available['text/html'] = proc { result }
-      expect(middleware.call(env)).to be result
+  context 'when configured with available values' do
+    subject(:middleware) do
+      described_class.new(default) do |negotiate|
+        negotiate.for('text/html', 'image/tiff')
+      end
+    end
+
+    %w( image/* audio/tone ).each do |type|
+      it 'calls the default application' do
+        result = double
+        expect(default).to receive(:call).with(env).and_return(result)
+        expect(middleware.call(env.merge!('HTTP_ACCEPT' => type))).to be result
+      end
+    end
+
+    it 'sets the negotiated header value' do
+      expect(default).to receive(:call) do |env|
+        expect(env['rack-content_negotiation.media_type']).to eq 'image/tiff'
+      end
+
+      middleware.call(env.merge('HTTP_ACCEPT' => 'image/*'))
+    end
+
+    context 'when there are no matching values' do
+      it 'does not set a negotiated header value' do
+        expect(default).to receive(:call) do |env|
+          expect(env['rack-content_negotiation.media_type']).to be nil
+        end
+
+        middleware.call(env.merge('HTTP_ACCEPT' => 'audio/tone'))
+      end
     end
   end
 
-  context 'when configured' do
+  context 'when configured with applications' do
     let(:html_application) { double(:html) }
     let(:tiff_application) { double(:tiff) }
     let(:result) { double }
 
     subject(:middleware) do
-      described_class.new(fallback) do |negotiate|
+      described_class.new(default) do |negotiate|
         negotiate.on('text/html', html_application)
         negotiate.on('image/tiff', tiff_application)
       end
@@ -83,23 +97,27 @@ RSpec.describe Rack::ContentNegotiation do
       expect(middleware.call(env.merge!('HTTP_ACCEPT' => 'image/*'))).to be result
     end
 
+    it 'sets the negotiated header value' do
+      expect(tiff_application).to receive(:call) do |env|
+        expect(env['rack-content_negotiation.media_type']).to eq 'image/tiff'
+      end
+
+      middleware.call(env.merge('HTTP_ACCEPT' => 'image/*'))
+    end
+
     context 'when there are no matching applications' do
-      it 'calls the fallback application' do
-        expect(fallback).to receive(:call).with(env).and_return(result)
+      it 'calls the default application' do
+        expect(default).to receive(:call).with(env).and_return(result)
         expect(middleware.call(env.merge!('HTTP_ACCEPT' => 'audio/tone'))).to be result
       end
+
+      it 'does not set a negotiated header value' do
+        expect(default).to receive(:call) do |env|
+          expect(env['rack-content_negotiation.media_type']).to be nil
+        end
+
+        middleware.call(env.merge('HTTP_ACCEPT' => 'audio/tone'))
+      end
     end
-  end
-
-  it 'sets the negotiated header value' do
-    called = false
-
-    middleware.on *%w( application/xhtml+xml text/html ) do |env|
-      called = true
-      expect(env['rack-content_negotiation.media_type']).to eq 'text/html'
-    end
-
-    middleware.call(env.merge('HTTP_ACCEPT' => 'text/*'))
-    expect(called).to be true
   end
 end
